c900170cad613ea030e0d70698397e6e60d7aeb7
Eddy Decena <eddynelson02@gmail.com>
2021-07-28 20:02:14 -0400

    add priority queue with binary heap

--- priority-queue/main.py
+++ priority-queue/main.py
@@ -23,16 +23,48 @@ class PriorityQueue():
             print()
             exit()
 
+class BinaryHeapPQ():
+    heap: list
+    N: int
+    
+    def __init__(self, capacity: int) -> None:
+        self.heap = [None] * (capacity + 1)
+        self.N = 0
+    
+    def insert(self, data) -> None:
+        self.N += 1
+        self.heap[self.N] = data
+        self.swim(self.N)
+        # print(self.heap)
+    
+    def delete_max(self) -> None:
+        pass
+    
+    def swim(self, k: int):
+        # print(k, self.heap[k//2], self.heap[k], self.less(k//2, k))
+        while (k > 1) and self.less(k//2, k):
+            self.swap(k//2, k)
+            k //= 2
+            print(k)
+    
+    def less(self, x: int, y: int):
+        if self.heap[x] < self.heap[y]:
+            return True
+        return False
+
+    def swap(self, i: int, min: int):
+        temp = self.heap[i]
+        self.heap[i] = self.heap[min]
+        self.heap[min] = temp
+
 if __name__ == '__main__':
-    pq: PriorityQueue = PriorityQueue()
-    pq.insert(8)
+    pq: BinaryHeapPQ = BinaryHeapPQ(10)
+    pq.insert(15)
     pq.insert(12)
     pq.insert(10)
-    pq.insert(9)
     pq.insert(11)
     pq.insert(20)
-    pq.insert(15)
-    pq.insert(5)
-    print(pq.queue)
-    print(pq.delete_max())
-    print(pq.queue)
+    pq.insert(11)
+    print(pq.heap)
+    # print(pq.delete_max())
+    # print(pq.queue)
