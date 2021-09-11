b5f8f4ec3b8d1a17f7d59afb87880837a7609c19
Eddy Decena <eddynelson02@gmail.com>
2021-09-11 18:05:44 -0400

    shortest path algo

--- breadth-first-search/graph/bfs.py
+++ breadth-first-search/graph/bfs.py
@@ -1,20 +1,25 @@
 from typing import Dict
 from typing import List
+from typing import Tuple
+from typing import Union
 
-def bfs(GRAPH: Dict[str, List[str]], start: str='A') -> List[str]:
-    queue: List[str] = []
+def bfs(GRAPH: Dict[str, List[str]], start: str='A', end: str='F', shortest_path: bool=False) -> Union[List[str], int]:
+    queue: List[Tuple[str, int]] = []
     visited: List[str] = []
 
-    queue.append(start)
+    queue.append((start, 0))
     visited.append(start)
 
     while len(queue) > 0:
         node: str = queue.pop(0)
-        print(node, end=" ")
+        print(node[0], end=" ")
         
-        for neighbour in GRAPH[node]:
+        if (node[0] == end) and shortest_path:
+            return node[1]
+            
+        for neighbour in GRAPH[node[0]]:
             if neighbour not in visited:
                 visited.append(neighbour)
-                queue.append(neighbour)
+                queue.append((neighbour, node[1] + 1))
     
     return visited
