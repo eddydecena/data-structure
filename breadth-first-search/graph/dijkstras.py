1246847dabcd9af976581f2a133caa7a5d45cddc
Eddy Decena <eddynelson02@gmail.com>
2021-09-26 16:30:43 -0400

    Create shortest path method with dijkstras implementation

--- breadth-first-search/graph/dijkstras.py
+++ breadth-first-search/graph/dijkstras.py
@@ -30,28 +30,46 @@ class LazyDijkstras():
     
     def add_edge(self, u: int=0, v: int=0, w: int=0) -> None:
         """
-        u: Node index
-        v: point to
+        u: Actual node
+        v: Edge to
         w: Edge weight
         """
         self.graph[u].append((v, w))
     
-    def run(self, start: int = 0) -> Tuple[Optional[int], Optional[int]]:
+    def run(self, start: int = 0, end: Optional[int]=None, early_stop: Optional[bool]=False) -> Tuple[List[int], List[int]]:
         dist: List[int] = [math.inf] * self.n
+        prev: List[int] = [None] * self.n
+        
         dist[start] = 0
         
         self.pq.put((start, 0))
         
         while not self.pq.empty():
-            index, _ = self.pq.get()
+            index, min_value = self.pq.get()
             self.visited[index] = True
+            if dist[index] < min_value: continue
             for edge in self.graph[index]:
-                if self.visited[edge[0]]:
-                    continue
-                
+                if self.visited[edge[0]]: continue
+                    
                 new_dist = dist[index] + edge[1]
                 
                 if new_dist < dist[edge[0]]:
+                    prev[edge[0]] = index
                     dist[edge[0]] = new_dist
                     self.pq.put((edge[0], new_dist))
-        return dist
+            if early_stop and (index == end):
+                return dist[end]
+    
+    def shortest_path(self, start: int, end: int):
+        dist, prev = self.run(start)
+        
+        path: List[int] = []
+        if (dist[end] == math.inf): return path
+        
+        at: int = end
+        while at != None:
+            path.append(at)
+            at = prev[at]
+        
+        path.reverse()
+        return path
